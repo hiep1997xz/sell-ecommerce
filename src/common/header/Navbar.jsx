eb2b08b45376116027f0ebe15a72a2a05b545216
hiep1997xz <hiep1997xz@gmail.com>
2022-10-24 20:37:13 +0700

    first commit

---
+++ src/common/header/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+const Navbar = () => {
+  const [mobileMenu, setMobileMenu] = useState(false)
+
+  return (
+    <>
+      <header className="header">
+        <div className="container d_flex">
+          <div className="catgrories d_flex">
+            <span className="fa-solid fa-border-all"></span>
+            <h4>
+              Categories <i className="fa fa-chevron-down"></i>
+            </h4>
+          </div>
+          <div className="navlink">
+            <ul
+              className={
+                mobileMenu ? 'nav-links-mobile' : 'link f_flex capitalize'
+              }
+              onClick={() => setMobileMenu(false)}>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/pages">Pages</Link>
+              </li>
+              <li>
+                <Link to="/user">User Account</Link>
+              </li>
+              <li>
+                <Link to="/vendor">Vendor Account</Link>
+              </li>
+              <li>
+                <Link to="/track">Track My Order</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+
+            <button
+              className="toggle"
+              onClick={() => setMobileMenu(!mobileMenu)}>
+              {mobileMenu ? (
+                <i className="fas fa-times close home-bth"></i>
+              ) : (
+                <i className="fa-solid fa-bars open"></i>
+              )}
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/components/mainpage/Slider.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SlideCart from './SlideCart'
+
+const Slider = () => {
+  return (
+    <>
+      <section className='homeSlide contentWidth'>
+        <div className='container'>
+          <SlideCart />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Slider
